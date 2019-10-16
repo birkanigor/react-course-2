30587b99661ee2eacfddcf8a0b33233ed09a40ca
igorbirkan <igor.birkan@gmail.com>
2019-10-16 22:20:30 +0300

    lesson 14

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile =() =>{
   return  (
-    <div className="content">
+    <div className={s.content}>
       <div>
         <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
       </div>
       <div>
         <img src="http://beyondplm.com/wp-content/uploads/2015/06/deep-linking-plm.jpg"></img>
       </div>
-      <div>
-        My posts
+      <div className={s.posts}>
+        <div className={s.item}>
+          My posts - 1
+        </div>
+        <div className={s.item}>
+          My posts - 1
+        </div>
       </div>
     </div>
   )
