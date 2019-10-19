1e870b9b2485c0445c8e9c3f67e2cc323d3729f3
igorbirkan <igor.birkan@gmail.com>
2019-10-19 15:09:56 +0300

    lesson-20

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import s from './Profile.module.css';
 
 const Profile =() =>{
   return  (
-    <div className={s.content}>
+    <div >
       <div>
         <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=700"></img>
       </div>
