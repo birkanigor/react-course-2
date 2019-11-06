255badbea0c093d76c96ed668947e7a43c48ca57
igorbirkan <igor.birkan@gmail.com>
2019-11-06 21:10:05 +0200

    Lesson-53

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,47 @@
 import React from 'react';
 import s from './Users.module.css'
+import * as axios from 'axios'
+import userPhoto from './../../assets/images/user_1.svg'
 
-let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-            { id: 1, photoUrl: "https://image.flaticon.com/icons/svg/145/145859.svg", followed: false, fullName: 'John Smith', status: 'I am new user', location: { country: 'England', city: 'London' } },
-            { id: 2, photoUrl: "https://image.flaticon.com/icons/svg/163/163801.svg", followed: true, fullName: 'Ruth M. Blackwell', status: 'Feeling great', location: { country: 'USA', city: 'Los Angeles' } },
-            { id: 3, photoUrl: "https://image.flaticon.com/icons/svg/163/163834.svg", followed: false, fullName: 'Adam Cohen', status: 'Hey there', location: { country: 'Israel', city: 'Tel Aviv' } }
-        ]);
+class Users extends React.Component{
+
+    constructor(props){
+        super(props);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response =>{
+            this.props.setUsers(response.data.items);
+        })
     }
 
-
-    return <div>
-        {
-            props.users.map(u => 
-                <div key={u.id} className={s.userCard}>
+    render() {
+        return <div>
+            {
+                this.props.users.map(u =>
+                    <div key={u.id} className={s.userCard}>
                     <span>
-                        <div>
-                            <img src={u.photoUrl} className={s.userPhoto} />
+                        <div className={s.userAvatar}>
+                            <img src={u.photos.small ?  u.photos.small : userPhoto } className={s.userPhoto} alt={"no img"} />
                         </div>
-                        <div>
+                        <div className={s.followUser}>
                             {u.followed ?
-                                <button onClick={() => { props.unFollow(u.id) }}>Unfollow</button> :
-                                <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+                                <button onClick={() => { this.props.unFollow(u.id) }}>Unfollow</button> :
+                                <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
 
                         </div>
                     </span>
-                    <span>
                         <span>
-                            <div><b><u>{u.fullName}</u></b></div><div>{u.status}</div>
+                        <span>
+                            <div >Name : <b><u>{u.name}</u></b></div><div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>Country : {(u.location && u.location.country) ?  u.location.country : 'USA'}</div>
+                            <div>City : {(u.location && u.location.city) ?  u.location.city : 'NY'}</div>
 
                         </span>
                     </span>
-                </div>
-            )
-        }
-    </div>
+                    </div>
+                )
+            }
+        </div>
+    }
 }
-
 export default Users;
